dc5a6557d87abfaca95323c12cc1028c8d751f83
jus0k <jutjd115@gmail.com>
2019-09-12 00:02:45 +0900

    practice useContext with translater

---
+++ src/context.js
@@ -0,0 +1,32 @@
+import React, { useState, useContext, createContext } from "react";
+
+const LangContext = createContext();
+
+const Lang = ({ defaultLang, children, translations }) => {
+  const [lang, setLang] = useState(defaultLang);
+  const hyperTranslate = text => {
+    if (lang === defaultLang) {
+      return text;
+    } else {
+      return translations[lang][text];
+    }
+  };
+  console.log();
+  return (
+    <LangContext.Provider value={{ setLang, hyperTranslate }}>
+      {children}
+    </LangContext.Provider>
+  );
+};
+
+export const useSetLang = lang => {
+  const { setLang } = useContext(LangContext);
+  return setLang;
+};
+
+export const useHyperTranslate = text => {
+  const { hyperTranslate } = useContext(LangContext);
+  return hyperTranslate;
+};
+
+export default Lang;
